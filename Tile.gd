a0c4a785da2aa4b7d27cc142a8fefc5c37484679
sirtau <contact@hamidmousa.com>
2021-08-22 18:23:53 +1000

    Added CanvasLayer, started Tile Script
    CanvasLayer/Camera nodes to place elements correctly
    Tile script started with a few functions to start building from.

--- Tile.gd
+++ Tile.gd
@@ -1,2 +1,24 @@
 extends Area2D
 
+export var startTile : bool = false
+
+var hasBuilding : bool = false
+var canPlaceBuilding : bool = false
+
+onready var highlight : Sprite = get_node("Highlight")
+onready var buildingIcon : Sprite = get_node("BuildingIcon")
+
+func _ready ():
+	add_to_group("Tiles")
+	
+func toggle_highlight (toggle):
+	highlight.visible = toggle
+	canPlaceBuilding = toggle
+
+func place_building (buildingTexture):
+	hasBuilding = true
+	buildingIcon.texture = buildingTexture
+
+
+func _on_Tile_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
